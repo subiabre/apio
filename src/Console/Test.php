318c9e5e007902e549a9d97dff415f772c9c43f3
Facu <subiabrewd@gmail.com>
2020-06-18 22:55:32 +0200

    Rename PHPUnit command to Test

--- src/Console/Test.php
+++ src/Console/Test.php
@@ -7,7 +7,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PHPUnit extends Command
+/**
+ * Run PHPUnit
+ */
+class Test extends Command
 {
     protected function configure()
     {
