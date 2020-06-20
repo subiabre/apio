8654d454562df67c5355b604870520a913d0de17
Facu <subiabrewd@gmail.com>
2020-06-20 02:41:05 +0200

    Remove unused use statement

--- src/Component/Bag.php
+++ src/Component/Bag.php
@@ -3,7 +3,6 @@
 namespace Apio\Component;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
