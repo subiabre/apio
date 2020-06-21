722a42173f0f71282a06b72f9340b5b8bb99e458
Facu <subiabrewd@gmail.com>
2020-06-21 22:14:51 +0200

    Override routes loader parent constructor

--- src/Routing/RoutesLoader.php
+++ src/Routing/RoutesLoader.php
@@ -4,6 +4,11 @@ namespace Apio\Routing;
 
 abstract class RoutesLoader extends Router implements RoutesLoaderInterface
 {
+    public function __construct()
+    {
+        // It makes no sense to let inject Request and Response interfaces here
+    }
+
     /**
      * Add new routes using the internal router instance
      */
