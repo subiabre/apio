592f36b7904e449ccbcfecbfcf55e90730e6835b
Facu <subiabrewd@gmail.com>
2020-06-22 07:31:29 +0200

    Update RoutesLoaderMock

--- tests/Routing/RoutesLoaderMock.php
+++ tests/Routing/RoutesLoaderMock.php
@@ -3,11 +3,12 @@
 namespace Apio\Tests\Routing;
 
 use Apio\Routing\AbstractRoutesLoader;
+use Apio\Routing\Response;
 
 class RoutesLoaderMock extends AbstractRoutesLoader
 {
     public function routes(): void
     {
-        $this->get('/test', fn() => 'OK');
+        $this->get('/test', fn(): Response => new Response);
     }
 }
