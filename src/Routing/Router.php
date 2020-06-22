eb65d43c02231938456af03b0a7fe354ba58874f
Facu <subiabrewd@gmail.com>
2020-06-22 23:13:41 +0200

    Update router doc blocs

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -10,8 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class Router
 {
+    /**
+     * The Request handler object instance
+     * @var Request
+     */
     public $request;
 
+    /**
+     * @param Request $request Request handler object
+     */
     public function __construct(Request $request = NULL)
     {
         if (!$request) $request = Request::createFromGlobals();
