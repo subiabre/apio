20d9690c65ae51cc526186ba562dedd04e9d9623
Facu <subiabrewd@gmail.com>
2020-06-23 18:35:11 +0200

    Make default route error handlers testeable

--- src/Routing/RouteList.php
+++ src/Routing/RouteList.php
@@ -8,31 +8,23 @@ namespace Apio\Routing;
  */
 class RouteList extends AbstractRouteList
 {
-    /**
-     * @codeCoverageIgnore
-     */
     public function methodNotAllowed(\Symfony\Component\HttpFoundation\Request $request, array $methods): \Apio\Routing\Response
     {
         $response = new Response();
 
         $response
             ->error(['message' => 'Route request method not allowed.'])
-            ->error(['methods' => $methods])
-            ->send();
+            ->error(['methods' => $methods]);
 
         return $response;
     }
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function routeNotFound(\Symfony\Component\HttpFoundation\Request $request): \Apio\Routing\Response
     {
         $response = new Response();
 
         $response
-            ->error(['message' => 'Route not found.'])
-            ->send();
+            ->error(['message' => 'Route not found.']);
 
         return $response;
     }
