a5f94c783ae9256f496496d81e92e02a252b56e6
Facu <subiabrewd@gmail.com>
2020-07-08 01:04:26 +0200

    Add server test

---
+++ tests/Http/ServerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Apio\Tests\Http;
+
+use Apio\Http\Response;
+use Apio\Http\Server;
+use Apio\Routing\Router;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class ServerTest extends TestCase
+{
+    public function testListenReturnsResponse()
+    {
+        $server = new Server;
+        $router = new Router;
+        $response = new Response;
+
+        $router->get('/', function(Request $request) use ($response){
+            return $response;
+        });
+
+        $serverResponse = $server->listen(3000, $router);
+
+        $this->assertSame($response, $serverResponse);
+    }
+}
