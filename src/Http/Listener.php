82f60931f7715ce1db0c7d4ca4efb4e449b41ce2
Facu <subiabrewd@gmail.com>
2020-07-08 07:14:05 +0200

    Add log at the end of loop

--- src/Http/Listener.php
+++ src/Http/Listener.php
@@ -30,11 +30,11 @@ class Listener
             yield $this->http->start();
 
             // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
-            Loop::onSignal(SIGINT, function (string $watcherId) use ($fn) {
+            Loop::onSignal(SIGINT, function (string $watcherId) {
+                $this->http->getLogger()->notice("Server connection closed.", ['watcher' => $watcherId]);
+
                 Loop::cancel($watcherId);
                 yield $this->http->stop();
-
-                $fn();
 
                 echo PHP_EOL;
             });
