ba342fc972bb5e5d77a0de6cc1952885020eaf91
Facu <subiabrewd@gmail.com>
2020-07-08 17:19:17 +0200

    Use RequestHandler as type

--- src/Http/Server.php
+++ src/Http/Server.php
@@ -3,8 +3,8 @@
 namespace Apio\Http;
 
 use Amp\Http\Server\HttpServer;
+use Amp\Http\Server\RequestHandler;
 use Amp\Socket\Server as SocketServer;
-use Apio\Routing\Router;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
@@ -16,15 +16,15 @@ class Server
     /**
      * Get an Amp server instance with a single socket
      * @param string $address Port number to bind the server socket to
-     * @param Router $router Instance of router
+     * @param RequestHandler $router Instance of router
      * @return Listener
      */
-    public function listen(string $address, Router $router, LoggerInterface $logger = NULL): Listener
+    public function listen(string $address, RequestHandler $router, LoggerInterface $logger = NULL): Listener
     {
         $sockets = [SocketServer::listen($address)];
         $logger = $logger ? $logger : new NullLogger;
 
-        $server = new HttpServer($sockets, $router->routes, $logger);
+        $server = new HttpServer($sockets, $router, $logger);
 
         return new Listener($server);
     }
